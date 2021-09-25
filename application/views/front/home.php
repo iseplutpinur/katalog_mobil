f696c42c37797044ce19d0aa54172e4cd282790d
AhmadYuunus <ahmadyuunus090901@gmail.com>
2021-09-25 16:47:24 +0700

    membuat front end home

---
+++ application/views/front/home.php
@@ -0,0 +1,338 @@
+<!-- hero area start  -->
+<section class="hero-area p-relative">
+    <div class="slider-active swiper-container">
+        <div class="swiper-wrapper">
+            <div class="single-slider slider-height st-2 swiper-slide slider-overlay" data-swiper-autoplay="5000">
+                <div class="slide-bg" data-background="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02u6Me4aR9nm040755g3a8929d.jpg"></div>
+                <div class="banner3-shape">
+                    <img src="<?= base_url() ?>assets/growbiz/img/shape/banner3-shape.png" alt="">
+                </div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-9">
+                            <div class="hero-content text-center">
+                                <p data-animation="fadeInUp" data-delay=".3s">Dealer Mitsubishi Malang</p>
+                                <h1 data-animation="fadeInUp" data-delay=".5s">SUN STAR MOTOR MALANG</h1>
+                                <!-- <div class="hero-content-btn st-2" data-animation="fadeInUp" data-delay=".7s">
+                                            <a href="contact.html" class="grb-btn">Get Started</a>
+                                        </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slider slider-height st-2 swiper-slide slider-overlay" data-swiper-autoplay="5000">
+                <div class="slide-bg" data-background="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02uniiCt06om171954g7ce0e17.jpg"></div>
+                <div class="banner3-shape">
+                    <img src="<?= base_url() ?>assets/growbiz/img/shape/banner3-shape.png" alt="">
+                </div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-9">
+                            <div class="hero-content text-center">
+                                <p data-animation="fadeInUp" data-delay=".3s">Dealer Mitsubishi Malang</p>
+                                <h1 data-animation="fadeInUp" data-delay=".5s">SUN STAR MOTOR MALANG</h1>
+                                <!-- <div class="hero-content-btn st-2" data-animation="fadeInUp" data-delay=".7s">
+                                            <a href="contact.html" class="grb-btn">Get Started</a>
+                                        </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slider slider-height st-2 swiper-slide slider-overlay" data-swiper-autoplay="5000">
+                <div class="slide-bg" data-background="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02uoPt5w748m040640g6383097.jpg"></div>
+                <div class="banner3-shape">
+                    <img src="<?= base_url() ?>assets/growbiz/img/shape/banner3-shape.png" alt="">
+                </div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-9">
+                            <div class="hero-content text-center">
+                                <p data-animation="fadeInUp" data-delay=".3s">Dealer Mitsubishi Malang</p>
+                                <h1 data-animation="fadeInUp" data-delay=".5s">SUN STAR MOTOR MALANG</h1>
+                                <!-- <div class="hero-content-btn st-2" data-animation="fadeInUp" data-delay=".7s">
+                                            <a href="contact.html" class="grb-btn">Get Started</a>
+                                        </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- If we need navigation buttons -->
+        <!-- <div class="slider-nav">
+                    <div class="swiper-button-prev"><i class="far fa-arrow-left"></i></div>
+                    <div class="swiper-button-next"><i class="far fa-arrow-right"></i></div>
+                </div> -->
+    </div>
+</section>
+<!-- hero area end -->
+<!-- choosing area start  -->
+<div class="choosing-fl-area">
+    <div class="container-fluid choosing-container-2">
+        <div class="row wow fadeInUp align-items-center">
+            <div class="col-lg-6">
+                <div class="choosing-fl-img p-relative text-center">
+                    <img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang052niioiaDm192114gef9cee8.jpg" alt="">
+                    <!-- <div class="choosing-video play_btn">
+                                <a class="grb-video st-3 popup-video"
+                                    href="https://www.youtube.com/watch?v=pNje3bWz7V8"><i class="fas fa-play"></i></a>
+                            </div> -->
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="choosing-fl-right mt-40 mb-40">
+                    <div class="section-title mb-30">
+                        <div class="border-left st-3">
+                            <p>Dealer Mitsubishi Malang</p>
+                        </div>
+                        <h4 class="white-color">Dapatkan Semua Informasi Lengkap Mobil Baru Mitsubishi Disini:
+                        </h4>
+                    </div>
+                    <div class="choosing__information st-2">
+                        <ul>
+                            <li>
+                                <div class="choosing__number">
+                                    <span>01</span>
+                                </div>
+                                <div class="choosing__text">
+                                    <h5>Pembelian Tunai atau Kredit</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="choosing__number">
+                                    <span>02</span>
+                                </div>
+                                <div class="choosing__text">
+                                    <h5>Program Promo Dealer</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="choosing__number">
+                                    <span>03</span>
+                                </div>
+                                <div class="choosing__text">
+                                    <h5>Simulasi/Perhitungan Paket Kredit Murah</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="choosing__number">
+                                    <span>04</span>
+                                </div>
+                                <div class="choosing__text">
+                                    <h5>Daftar Harga Terbaik</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="choosing__number">
+                                    <span>05</span>
+                                </div>
+                                <div class="choosing__text">
+                                    <h5>Test Drive</h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="choosing__number">
+                                    <span>06</span>
+                                </div>
+                                <div class="choosing__text">
+                                    <h5>Tukar Tambah</h5>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <br>
+                    <div class="section-title mb-30">
+                        <div class="border-left st-3">
+                            <p>Proses Pembelian Bisa Dilakukan Dirumah Tanpa Keluar Rumah, Diproses Cepat, Mudah
+                                dan Aman</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- choosing area end -->
+<!-- blog area start  -->
+<section class="blog-area blog-area-2 pt-120">
+    <div class="container">
+        <div class="row wow fadeInUp justify-content-center counter-head">
+            <div class="col-lg-6 col-md-8">
+                <div class="blog-left">
+                    <div class="section-title mb-55 text-center">
+                        <div class="border-c-bottom st-2">
+                            <p>Katalog Promo</p>
+                        </div>
+                        <h4>Informasi Program Promo September 2021 Pembelian Mobil Baru Mitsubishi</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row wow fadeInUp">
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <!-- <div class="blog-date st-2">
+                                <i class="fal fa-calendar-alt"></i>
+                                <span>22</span>
+                                <p>Jan 2020</p>
+                            </div> -->
+                    <div class="blog-img">
+                        <a href="new_xpander.html"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02ua0iDrM76huiamci04064670c9b.jpg" alt=""></a>
+                    </div>
+                    <div class="blog-content st-2">
+                        <!-- <ul class="blog-meta pl-100">
+                                    <li>
+                                        <i class="fal fa-user"></i>
+                                        <span class="author"><a href="new_xpander.html">Admin</a></span>
+                                    </li>
+                                    <li>
+                                        <i class="fal fa-comments"></i>
+                                        <span><a href="blog-details.html">03 Comments</a></span>
+                                    </li>
+                                </ul> -->
+                        <h4><a href="new_xpander.html">NEW XPANDER <i class="fas fa-arrow-right"></i></a></h4>
+                        <ul>
+                            <li>Harga mulai <b>Rp 232.200.000</b></li>
+                            <li>Kredit mulai <b>Rp 30 Jutaan</b></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="new_xpander.html"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02ucimaab7s82irM3a040646ac91b.jpg" alt=""></a>
+                    </div>
+                    <div class="blog-content st-2">
+                        <h4><a href="new_xpander.html">NEW XPANDER <i class="fas fa-arrow-right"></i></a></h4>
+                        <ul>
+                            <li>Harga mulai <b>Rp 232.200.000</b></li>
+                            <li>Kredit mulai <b>Rp 30 Jutaan</b></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="new_xpander.html"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02uhasimor2byMias5040646810de.jpg" alt=""></a>
+                    </div>
+                    <div class="blog-content st-2">
+                        <h4><a href="new_xpander.html">NEW XPANDER <i class="fas fa-arrow-right"></i></a></h4>
+                        <ul>
+                            <li>Harga mulai <b>Rp 232.200.000</b></li>
+                            <li>Kredit mulai <b>Rp 30 Jutaan</b></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="new_xpander.html"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02ua0iDrM76huiamci04064670c9b.jpg" alt=""></a>
+                    </div>
+                    <div class="blog-content st-2">
+                        <h4><a href="new_xpander.html">NEW XPANDER <i class="fas fa-arrow-right"></i></a></h4>
+                        <ul>
+                            <li>Harga mulai <b>Rp 232.200.000</b></li>
+                            <li>Kredit mulai <b>Rp 30 Jutaan</b></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="new_xpander.html"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02ucimaab7s82irM3a040646ac91b.jpg" alt=""></a>
+                    </div>
+                    <div class="blog-content st-2">
+                        <h4><a href="new_xpander.html">NEW XPANDER <i class="fas fa-arrow-right"></i></a></h4>
+                        <ul>
+                            <li>Harga mulai <b>Rp 232.200.000</b></li>
+                            <li>Kredit mulai <b>Rp 30 Jutaan</b></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="new_xpander.html"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang02uhasimor2byMias5040646810de.jpg" alt=""></a>
+                    </div>
+                    <div class="blog-content st-2">
+                        <h4><a href="new_xpander.html">NEW XPANDER <i class="fas fa-arrow-right"></i></a></h4>
+                        <ul>
+                            <li>Harga mulai <b>Rp 232.200.000</b></li>
+                            <li>Kredit mulai <b>Rp 30 Jutaan</b></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- blog area end -->
+<!-- blog area start  -->
+<section class="blog-area blog-area-2 pt-120">
+    <div class="container">
+        <div class="row wow fadeInUp justify-content-center counter-head">
+            <div class="col-lg-6 col-md-8">
+                <div class="blog-left">
+                    <div class="section-title mb-55 text-center">
+                        <div class="border-c-bottom st-2">
+                            <p>Video dan Galeri Foto</p>
+                        </div>
+                        <h4>Informasi Video Presentasi Terbaru serta Galeri Foto Mobil Baru Mitsubishi</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row wow fadeInUp">
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="#"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang052niioiaDm192114gef9cee8.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="#"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang022nyiAb0Cm172039g87a13e1.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="#"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang024yDibMram051832g0d1e136.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="#"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang025y3D8aPim051831g7e8524c.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="#"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang026obCMi7Am051810ga4bc087.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-single mb-30 p-relative st-2">
+                    <div class="blog-img">
+                        <a href="#"><img src="<?= base_url() ?>assets/growbiz/img/konten/mitsubishimalang028ber3Ctwm172040g75f367e.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- blog area end -->
---
+++ application/views/front/temp/footer.php
@@ -0,0 +1,45 @@
+</main>
+<!-- footer area start  -->
+<footer>
+    <div class="copyright-area st-2">
+        <div class="container">
+            <div class="row wow fadeInUp align-items-center">
+                <div class="d-md-flex copyright-text">
+                    <div class="p-2 flex-grow-1 copyright-text">
+                        <div class="copyright-text st-2">
+                            <p>Copyrighted by <a href="#">@Bdevs</a> | All Right Reserved</p>
+                        </div>
+                    </div>
+                    <div class="p-2 copyright-text">
+                        <i class="fas fa-phone-alt"></i> <span><a class="text-white" href="#">0812-3505-5522</a></span>
+                    </div>
+                    <div class="p-2 copyright-text"><i class="fas fa-phone-alt"></i> <span><a class="text-white" href="#">info@domain.com</a></span></div>
+                    <div class="p-2 copyright-text"><i class="fas fa-map-marker-alt"></i> <span><a class="text-white" href="#">Jl. Letjend S. Parman No 104A Malang, Jawa
+                                Timur 65122</a></span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer area end -->
+
+
+
+<!-- JS here -->
+<script src="<?= base_url() ?>assets/growbiz/js/vendor/jquery-3.6.0.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/swiper-bundle.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/isotope.pkgd.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/jquery.meanmenu.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/ajax-form.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/wow.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/jquery.scrollUp.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/odometer.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/appair.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/imagesloaded.pkgd.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/jquery.magnific-popup.min.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/plugins.js"></script>
+<script src="<?= base_url() ?>assets/growbiz/js/main.js"></script>
+</body>
+
+</html>
