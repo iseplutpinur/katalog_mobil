fbf9f4613ff5acdcf7a18682238797e1384f6894
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-09-25 22:11:53 +0700

    update header

--- application/views/admin/sitemain/header.php
+++ application/views/admin/sitemain/header.php
@@ -65,7 +65,7 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
-                <!-- <i class="fas fa-code"></i> -->
+                <i class="fas fa-car"></i>
                 <div class="sidebar-brand-text mx-3">KATALOG MOBIL</div>
             </a>
 
@@ -128,8 +128,6 @@
                     </div>
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <div class="topbar-divider d-none d-sm-block"></div>
-
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -138,13 +136,6 @@
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <?php
-                                if ($user['role_id'] == 1) {
-                                    $role_id = 'administrator';
-                                } elseif ($user['role_id'] == 2) {
-                                    $role_id = 'user';
-                                }
-                                ?>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
