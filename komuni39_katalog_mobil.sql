cbfe435931cdf090146083f7ba1f337affdc3fb1
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-09-27 15:43:28 +0700

    update database

--- komuni39_katalog_mobil.sql
+++ komuni39_katalog_mobil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2021 at 02:54 AM
+-- Generation Time: Sep 27, 2021 at 10:43 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.21
 
